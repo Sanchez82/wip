dbe43fcf85edd8046f8e6c2911e550316314a2d0
wisegar <yariel.re@wisegar.ch>
2017-07-11 23:21:50 +0200

    Added Wip.Core Project. Project order.

---
+++ Wip.Core/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wip.Core.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string  Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
