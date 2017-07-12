1014fa0948716587ab24e66217d4fdf876175e00
Yariel Rodriguez <yariel.re@wisegar.ch>
2017-07-12 10:24:47 +0200

    Added Options and configuration file

--- Wip.Web/Startup.cs
+++ Wip.Web/Startup.cs
@@ -8,15 +8,45 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.IO;
+using Wip.Core.Models;
+using Microsoft.Extensions.Configuration;
+using Wip.Web;
 
 namespace WebWip
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+           .SetBasePath(env.ContentRootPath)
+           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+           .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+           .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+            
+            // Adds services required for using options.
+            services.AddOptions();
+
+            // Register the IConfiguration instance which MyOptions binds against.
+            services.Configure<WipOptions>(Configuration);
+
+            // Registers the following lambda used to configure options.
+            services.Configure<WipOptions>(wipOptions =>
+            {
+                wipOptions.DefaultConnection = "Server=(localdb)\\MSSQLLocalDB;Database=_CHANGE_ME;Trusted_Connection=True;MultipleActiveResultSets=true";
+            });
+
             services.AddMvc();
+            services.AddDbContext<WipContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +68,7 @@ namespace WebWip
                     await next();
                 }
             });
+            
             app.UseMvcWithDefaultRoute();
             app.UseDefaultFiles();
             app.UseStaticFiles();
---
+++ Wip.Web/WipOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wip.Web
+{
+    public class WipOptions
+    {
+        public string DefaultConnection { get; set; }
+        public WipOptions()
+        {
+            // Set default value.
+            DefaultConnection = "Server=(localdb)\\MSSQLLocalDB;Database=_CHANGE_ME;Trusted_Connection=True;MultipleActiveResultSets=true";
+        }
+
+    }
+}
