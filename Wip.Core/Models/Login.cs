71e7b493929b98fd71eaf5319dfc92af34825d13
wisegar <yariel.re@wisegar.ch>
2017-07-12 07:28:23 +0200

    Added Login Model and edited User Model.

---
+++ Wip.Core/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wip.Core.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string HostName { get; set; }
+        public long LoggedTime { get; set; }
+    }
+}
--- Wip.Core/Models/User.cs
+++ Wip.Core/Models/User.cs
@@ -6,10 +6,12 @@ namespace Wip.Core.Models
 {
     public class User
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Lastname { get; set; }
         public string  Username { get; set; }
         public string Password { get; set; }
+        public ICollection<Login> LoginRecord { get; set; }
 
     }
 }
