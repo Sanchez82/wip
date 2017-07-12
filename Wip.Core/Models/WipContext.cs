7b61d703fe64d04da6022f5f8f74bad62edb3c93
Yariel Rodriguez <yariel.re@wisegar.ch>
2017-07-12 10:19:56 +0200

    Added Database context (WipContext)

---
+++ Wip.Core/Models/WipContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wip.Core.Models
+{
+    public class WipContext : DbContext
+    {
+        public WipContext() { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Login> Logins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(""); 
+        }
+    }
+}
